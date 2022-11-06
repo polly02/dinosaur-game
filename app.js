7c7e6b191ba430cda22ce635eb0b7ebcb70cc628
polina <polina.zdanovich.02@mail.ru>
2022-11-06 17:03:22 +0300

    start js

---
+++ app.js
@@ -0,0 +1,19 @@
+const dino = document.querySelector(".dino");
+const cactus = document.querySelector(".cactus");
+
+document.addEventListener("keydown", (event) => {
+    jump()
+});
+
+function jump() {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump")
+    }
+    setTimeout(function () {
+        dino.classList.remove("jump")
+    }, 300)
+}
+
+let isAlive = setInterval(function(){
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+})
