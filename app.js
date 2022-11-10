213f8d933b3256bfaf5130420161303b9acddda1
polina <polina.zdanovich.02@mail.ru>
2022-11-10 22:34:58 +0300

    working functionality

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const dino = document.querySelector(".dino");
-const cactus = document.querySelector(".cactus");
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
 
 document.addEventListener("keydown", (event) => {
     jump()
@@ -8,12 +8,17 @@ document.addEventListener("keydown", (event) => {
 function jump() {
     if (dino.classList != "jump") {
         dino.classList.add("jump")
+
+        setTimeout(function () {
+            dino.classList.remove("jump")
+        }, 300)
     }
-    setTimeout(function () {
-        dino.classList.remove("jump")
-    }, 300)
 }
 
-let isAlive = setInterval(function(){
+let isAlive = setInterval(function () {
     let dinotop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
-})
+    let cactusleft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+    if (cactusleft < 50 && cactusleft > 0 && dinotop >= 140){
+        alert("Game over")
+    }
+}, 10)
